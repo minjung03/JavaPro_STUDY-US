ffae07feef99ef69bb8f3a1539020312430b1549
minjung Kim <ikwmjj@naver.com>
2021-12-04 23:22:35 +0900

    12/4 소스 합침 & 디자인 수정

--- Page/SelectTimeTablePage.java
+++ Page/SelectTimeTablePage.java
@@ -9,21 +9,34 @@ import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.TextArea;
 
 import javax.swing.JCheckBox;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 
-public class SelectTimeTablePage extends JFrame {
-
-   private JPanel contentPane;
+public class SelectTimeTablePage extends JFrame implements ItemListener {
    
+   public static String select_time;
+   Font font1 = new Font("Cafe24SsurroundAir", Font.BOLD, 20);
+   Font font_10 = new Font("Cafe24SsurroundAir", Font.BOLD, 12);
+   Font font_14 = new Font("Cafe24SsurroundAir", Font.BOLD, 14);
+   
+   private JPanel contentPane;
+
 //   public static void main(String[] args) {
 //      EventQueue.invokeLater(new Runnable() {
 //         public void run() {
@@ -38,178 +51,184 @@ public class SelectTimeTablePage extends JFrame {
 //   }
 
    public SelectTimeTablePage() {
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
-         setResizable(false); //사이즈 변경 불가능
-         setVisible(true); //보이게 할지 여부
-         setBounds(100, 100, 1300, 800);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setResizable(false); // 사이즈 변경 불가능
+      setVisible(true); // 보이게 할지 여부
+      setBounds(100, 100, 1300, 800);
       contentPane = new JPanel();
       contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
       contentPane.setLayout(new BorderLayout(0, 0));
       setContentPane(contentPane);
       
-      TextArea ta;
-      
       JPanel panel = new JPanel();
+      panel.setBackground(new Color(255,255,255));
       contentPane.add(panel, BorderLayout.CENTER);
       panel.setLayout(null);
       
-      
-      JPanel subtitle_panel = new JPanel();
-      subtitle_panel.setLayout(null);
-      subtitle_panel.setForeground(new Color(211, 211, 211));
-      subtitle_panel.setBorder(new LineBorder(new Color(192, 192, 192), 30, true));
-      subtitle_panel.setBounds(0, 0, 1286, 52);
-      panel.add(subtitle_panel);
-      
-      JLabel subtitle_text = new JLabel("STUDY US");
-      subtitle_text.setForeground(Color.WHITE);
-      subtitle_text.setFont(new Font("Century Gothic", Font.PLAIN, 25));
-      subtitle_text.setBounds(27, 4, 440, 42);
-      subtitle_panel.add(subtitle_text);
-      
-      JPanel timetable_title_panel = new JPanel();
-      timetable_title_panel.setBounds(101, 96, 196, 39);
-      
-      panel.add(timetable_title_panel);
-      timetable_title_panel.setLayout(null);
-      timetable_title_panel.setBorder(new LineBorder(new Color(184,184,184), 30, true));
-      
-      JLabel timetable_title_text = new JLabel("\uC2DC\uAC04\uD45C \uC120\uD0DD");
+     JPanel subtitle_panel = new JPanel();
+     subtitle_panel.setLayout(null);
+     subtitle_panel.setForeground(new Color(211, 211, 211));
+     subtitle_panel.setBorder(new LineBorder(new Color(215,176,212), 30, true));
+     subtitle_panel.setBounds(0, 0, 1273, 52);
+     panel.add(subtitle_panel);
+     
+     JLabel title = new JLabel("STUDY US");
+     title.setForeground(new Color(255, 255, 255));
+     title.setFont(new Font("Century Gothic", Font.PLAIN, 25));
+     title.setBounds(22, 6, 440, 42);
+     subtitle_panel.add(title);
+     
+     JPanel flower_img_panel = new JPanel();
+     flower_img_panel.setBounds(130, 12, 40, 30);
+     subtitle_panel.add(flower_img_panel);
+     flower_img_panel.setLayout(null);
+     
+     JPanel timetable_title_panel = new JPanel();
+     timetable_title_panel.setBounds(101, 85, 196, 39);   
+     panel.add(timetable_title_panel);
+     timetable_title_panel.setLayout(null);
+     timetable_title_panel.setBorder(new LineBorder(new Color(127,114,165), 30, true));
+     
+     //패널에 그림을 올려주는 클래스
+     class ImagePanel extends JPanel {
+	     private Image img;
+	     
+	     public ImagePanel(Image img) {
+	         this.img = img;
+	         setSize(new Dimension(img.getWidth(null), img.getHeight(null)));
+	         setPreferredSize(new Dimension(img.getWidth(null), img.getHeight(null)));
+	         setLayout(null);
+	     }
+	     public void paintComponent(Graphics g) {
+	         g.drawImage(img, 3, 0, null);
+	        }
+     } 
+
+     ImagePanel flowerimg = new ImagePanel(new ImageIcon("./img/resizeflower.png").getImage());
+     flower_img_panel.add(flowerimg);
+         
+
+      JLabel timetable_title_text = new JLabel("시간제 선택");
       timetable_title_text.setForeground(Color.WHITE);
-      timetable_title_text.setFont(new Font("±¼¸²", Font.PLAIN, 20));
-      timetable_title_text.setBounds(45, 7, 105, 25);
+      timetable_title_text.setFont(font1);
+      timetable_title_text.setBounds(45, 7, 110, 25);
       timetable_title_panel.add(timetable_title_text);
-      
+
       JPanel timetable_panel = new JPanel();
       timetable_panel.setBackground(Color.WHITE);
-      timetable_panel.setBorder(new LineBorder(new Color(192, 192, 192), 3, true));
+      timetable_panel.setBorder(new LineBorder(new Color(127,114,165), 1, true));
       timetable_panel.setBounds(72, 96, 1109, 590);
       panel.add(timetable_panel);
       timetable_panel.setLayout(null);
 
-      
       JPanel coupon_name_p[] = new JPanel[4];
-      for(int i=0; i<4; i++) {
+      for (int i = 0; i < 4; i++) {
          coupon_name_p[i] = new JPanel();
          coupon_name_p[i].setLayout(null);
          coupon_name_p[i].setForeground(Color.WHITE);
-         coupon_name_p[i].setBorder(new LineBorder(new Color(192, 192, 192), 30, true));
+         // coupon_name_p[i].setBorder(new LineBorder (new Color(182,215,168), 30, true));
+ 
+         coupon_name_p[i].setBorder(new LineBorder (new Color(215,176,212), 30, true));
          timetable_panel.add(coupon_name_p[i]);
       }
-      
+
       JLabel coupon_name_l[] = new JLabel[4];
-      String[] cou_text = {"당일 이용권", "정액권", "기간제", "기타"};
-      for(int i=0; i<4; i++) {
+      String[] cou_text = { "당일 이용권", "정액권", "기간제", "기타" };
+      for (int i = 0; i < 4; i++) {
          coupon_name_l[i] = new JLabel(cou_text[i]);
          coupon_name_l[i].setForeground(Color.WHITE);
-         coupon_name_l[i].setFont(new Font("±¼¸²", Font.PLAIN, 14));
+         coupon_name_l[i].setFont(font_14);
          coupon_name_p[i].add(coupon_name_l[i]);
       }
-      
-      
-      
-   
-      
-      
       coupon_name_p[0].setBounds(173, 74, 124, 35);
-      
       coupon_name_l[0].setBounds(23, 7, 93, 25);
-      
+
       coupon_name_p[1].setBounds(409, 74, 124, 35);
       coupon_name_l[1].setBounds(40, 7, 53, 25);
-      
-      
+
       coupon_name_p[2].setBounds(650, 74, 124, 35);
-      
       coupon_name_l[2].setBounds(40, 7, 43, 25);
-      
+
       coupon_name_p[3].setBounds(861, 74, 124, 35);
       coupon_name_l[3].setBounds(46, 7, 29, 25);
-      
-      
+
       ButtonGroup group = new ButtonGroup();
       JRadioButton radio_time[] = new JRadioButton[13];
-      String[] rdo_text = {"2시간", "4시간", "6시간", "8시간", "50시간", "100시간", "150시간", "1일", "1주", "2주", "4주", "회의룸", "개인실"}; 
-      
-      for(int i=0; i<13; i++) {
-         
+      String[] rdo_text = { "2시간", "4시간", "6시간", "8시간", "50시간", "100시간", "150시간", "1일", "1주", "2주", "4주", "회의룸",
+            "개인실" };
+
+      for (int i = 0; i < 13; i++) {
          radio_time[i] = new JRadioButton(rdo_text[i]);
-         radio_time[i].setFont(new Font("±¼¸²", Font.BOLD, 20));
+         radio_time[i].setFont(font1);
          radio_time[i].setBackground(Color.white);
+         radio_time[i].addItemListener(this); // 이벤트 호출
          group.add(radio_time[i]);
          timetable_panel.add(radio_time[i]);
       }
-      
+
       int height_Default = 180;
-      for(int i = 0; i<4; i++) {
+      for (int i = 0; i < 4; i++) {
          radio_time[i].setBounds(190, height_Default, 86, 23);
-         height_Default+=70;
+         height_Default += 70;
       }
-      
+
       height_Default = 180;
-      for(int i = 4; i<7; i++) {
+      for (int i = 4; i < 7; i++) {
          radio_time[i].setBounds(418, height_Default, 109, 23);
-         height_Default+=70;
+         height_Default += 70;
       }
-   
-      
+
       height_Default = 180;
-      for(int i = 7; i<11; i++) {
+      for (int i = 7; i < 11; i++) {
          radio_time[i].setBounds(669, height_Default, 86, 23);
-         height_Default+=70;
+         height_Default += 70;
       }
-   
+
       height_Default = 180;
-      for(int i =11; i<13; i++) {
+      for (int i = 11; i < 13; i++) {
          radio_time[i].setBounds(883, height_Default, 102, 23);
-         height_Default+=70;
+         height_Default += 70;
       }
-   
-      
-      
-      JButton btn_Price = new JButton("\uAC00\uACA9\uD45C \uD655\uC778");
-      btn_Price.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) {
-         }
-      });
-      
+
+      JButton btn_Price = new JButton("가격표 보기");
       btn_Price.setForeground(Color.WHITE);
+      btn_Price.setFont(font_10);
       btn_Price.setBorder(new LineBorder(new Color(0, 0, 0), 0, true));
-      btn_Price.setBackground(Color.BLACK);
+      btn_Price.setBackground(new Color(0,0,0));
       btn_Price.setBounds(829, 500, 97, 28);
       timetable_panel.add(btn_Price);
-      
+
       btn_Price.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-               new PricePage(); // JoinPage 실행
-               setVisible(false);  // 창 안보이게 하기 
-            }
-         });
-            
-      
-      JButton btn_Chk = new JButton("\uC120\uD0DD \uC644\uB8CC");
-      btn_Chk.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
-         }
+            new PricePage(); // JoinPage 실행
+            setVisible(false); // 창 안보이게 하기
+         }   
       });
+
+      JButton btn_Chk = new JButton("선택 완료");
       btn_Chk.setForeground(Color.WHITE);
+      btn_Chk.setFont(font_10);
       btn_Chk.setBorder(new LineBorder(new Color(0, 0, 0), 0, true));
-      btn_Chk.setBackground(Color.BLACK);
+      btn_Chk.setBackground(new Color(0,0,0));
       btn_Chk.setBounds(938, 500, 97, 28);
       timetable_panel.add(btn_Chk);
-      
+
       btn_Chk.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-               new SeatPage(); // JoinPage 실행
-               setVisible(false);  // 창 안보이게 하기 
-            }
-         });
-      
-      
+         public void actionPerformed(ActionEvent e) {
+            new SelectSeatPage(); // JoinPage 실행
+            setVisible(false); // 창 안보이게 하기
+         }
+      });
+
+   }
+   public static void main(String[] args) {
+      new SelectTimeTablePage();
    }
 
-   
-      public static void main(String[] args) {
-          new SelectTimeTablePage();
-       }
+   public void itemStateChanged(ItemEvent e) {
+      JRadioButton jb = (JRadioButton) e.getSource();
+      if (jb.isSelected()) {
+    	  select_time = jb.getText();
+      }
    } 
+}
--- Page/StartPage.java
+++ Page/StartPage.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,7 +18,12 @@ public class StartPage extends JFrame {
    
    private JPanel contentPane, panel;
    private JButton btn_login, btn_join;
-   private JLabel title;
+   private JLabel title, sub_title;
+    
+   //폰트적용
+   Font font_75 = new Font("Cafe24SsurroundAir", Font.PLAIN, 75); //첫화면 제목 폰트
+   Font font_12 = new Font("Cafe24SsurroundAir", Font.BOLD, 12); 
+   Font font_17 = new Font("Cafe24SsurroundAir", Font.BOLD, 17); 
    
    StartPage(){
       super("스터디카페 예약 프로그램 [STUDY US]");
@@ -41,16 +47,26 @@ public class StartPage extends JFrame {
       contentPane.add(panel, BorderLayout.CENTER);
       panel.setLayout(null);
       
+      sub_title = new JLabel("스터디 카페 관리 프로그램");
+      sub_title.setFont(font_17);
+      sub_title.setForeground(new Color(127,114,165));
+      sub_title.setHorizontalAlignment(SwingConstants.CENTER);
+      sub_title.setBounds(132, 206, 624, 175);//624
+      panel.add(sub_title);
+      
       title = new JLabel("STUDY US");
+      title.setFont(font_75);
+      title.setForeground(new Color(127,114,165));
       title.setHorizontalAlignment(SwingConstants.CENTER);
-      title.setFont(new Font("Century Gothic", Font.PLAIN, 70));
-      title.setBounds(132, 296, 624, 175);
+      title.setBounds(132, 266, 624, 175);//624
       panel.add(title);
       
       btn_login = new JButton("로그인");
+      btn_login.setFont(font_12);
       btn_login.setForeground(Color.WHITE);
-      btn_login.setBounds(813, 448, 97, 23);
-      btn_login.setBackground(Color.BLACK);
+      btn_login.setBorderPainted(false);	//테두리 없애기
+      btn_login.setBounds(900, 448, 97, 28);
+      btn_login.setBackground(new Color(215,176,212));
       panel.add(btn_login);
       
       btn_login.addActionListener(new ActionListener() {
@@ -59,12 +75,12 @@ public class StartPage extends JFrame {
             setVisible(false);  // 창 안보이게 하기 
          }
       });
-      
-      
       btn_join = new JButton("회원가입");
+      btn_join.setFont(font_12);
       btn_join.setForeground(Color.WHITE);
-      btn_join.setBackground(Color.BLACK);
-      btn_join.setBounds(813, 481, 97, 23);
+      btn_join.setBorderPainted(false);	//테두리 없애기
+      btn_join.setBackground(new Color(215,176,212));
+      btn_join.setBounds(900, 481, 97, 28);
       panel.add(btn_join);
       
       btn_join.addActionListener(new ActionListener() {
@@ -73,11 +89,9 @@ public class StartPage extends JFrame {
             setVisible(false);  // 창 안보이게 하기 
          }
       });
-   
    }
    
    public static void main(String[] args) {
       new StartPage();
    }
-
 }
