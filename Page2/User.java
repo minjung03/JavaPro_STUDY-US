f93c93754ad243fafba6d07ff0c47ffa3b88a546
서민정 <79045880+Smj04@users.noreply.github.com>
2021-12-05 17:27:42 +0900

    (서민) 12/5 user.java수정

--- Page2/User.java
+++ Page2/User.java
@@ -5,7 +5,9 @@ public class User {
 	private String name;
 	private String id;
 	private String pass;
-	private String regular_chk;
+	private String time_chk;
+	private String selected_time;
+	private String selected_seat;
 	
 	public String getName() {
 		return name;
@@ -25,13 +27,23 @@ public class User {
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
-	public String getRegular_chk() {
-		return regular_chk;
+	public String getTime_chk() {
+		return time_chk;
 	}
-	public void setRegular_chk(String regular_chk) {
-		this.regular_chk = regular_chk;
+	public void setTime_chk(String time_chk) {
+		this.time_chk = time_chk;
+	}
+	public String getSelected_time() {
+		return selected_time;
+	}
+	public void setSelected_time(String selected_time) {
+		this.selected_time = selected_time;
+	}
+	public String getSelected_seat() {
+		return selected_seat;
+	}
+	public void setSelected_seat(String selected_seat) {
+		this.selected_seat = selected_seat;
 	}
 	
-	
-
 }
